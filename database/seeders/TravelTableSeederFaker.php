8bd0988e2a9a30d57fae60af5b87437ed96a8aaa
Vinctdea <vincenzo.deangelis23@gmail.com>
2024-09-15 15:31:53 +0200

    ticket

--- database/seeders/TravelTableSeederFaker.php
+++ database/seeders/TravelTableSeederFaker.php
@@ -19,12 +19,13 @@ class TravelTableSeederFaker extends Seeder
         for ($i = 0; $i < 50; $i++) {
 
             $new_travel = new Travel();
-            $new_travel->destinazione = $faker->words(100);
-            $new_travel->luogo_partenza = $faker->words(255);
-            $new_travel->luogo_arrivo = $faker->words(255);
+            $new_travel->destinazione = $faker->word(99);
+            $new_travel->luogo_partenza = $faker->word(255);
+            $new_travel->luogo_arrivo = $faker->word(255);
             $new_travel->prezzo = $faker->numberBetween(500, 100000);
-            $new_travel->itinerario = $faker->words(255);
-            $new_travel->img_url = $faker->words(255);
+            $new_travel->itinerario = $faker->word(255);
+            $new_travel->img_url = $faker->word(255);
+            $new_travel->save();
         }
     }
 }
